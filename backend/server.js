fae877bf732b70757ee03edb425e8759b1d166bd
unknown <kinantisaaz@gmail.com>
2023-08-12 18:05:54 +0700

    cors

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,6 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 // connect db
 connectDatabase();
 
-// cloudinary.config({
-//   cloud_name: process.env.CLOUDINARY_NAME,
-//   api_key: process.env.CLOUDINARY_API_KEY,
-//   api_secret: process.env.CLOUDINARY_API_SECRET
-// })
-
-
 // create server
 const server = app.listen(process.env.PORT, () => {
   console.log(
