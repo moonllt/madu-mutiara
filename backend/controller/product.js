230b2f857323226c36ea4604151b5de2ff5834c0
unknown <kinantisaaz@gmail.com>
2023-08-16 17:48:07 +0700

    update controller dan redux action product

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -41,9 +41,9 @@ const cloudinary = require('cloudinary').v2;
 // );
 
 
+// create product
 router.post(
   "/create-product",
-  upload.array("images"),
   catchAsyncErrors(async (req, res, next) => {
     try {
       const shopId = req.body.shopId;
@@ -51,23 +51,29 @@ router.post(
       if (!shop) {
         return next(new ErrorHandler("Shop Id is invalid!", 400));
       } else {
-        const files = req.files;
+        let images = [];
 
-        const imageUrls = [];
-
-        for (const file of files) {
-  const result = await cloudinary.uploader.upload(file.path, {
-    public_id: `product_image_${Date.now()}`, // Menggunakan timestamp sebagai public_id
-  });
-  imageUrls.push({
-    public_id: result.public_id,
-    url: result.secure_url,
-  });
-}
-
-
+        if (typeof req.body.images === "string") {
+          images.push(req.body.images);
+        } else {
+          images = req.body.images;
+        }
+      
+        const imagesLinks = [];
+      
+        for (let i = 0; i < images.length; i++) {
+          const result = await cloudinary.v2.uploader.upload(images[i], {
+            folder: "products",
+          });
+      
+          imagesLinks.push({
+            public_id: result.public_id,
+            url: result.secure_url,
+          });
+        }
+      
         const productData = req.body;
-        productData.images = imageUrls;
+        productData.images = imagesLinks;
         productData.shop = shop;
 
         const product = await Product.create(productData);
@@ -78,30 +84,11 @@ router.post(
         });
       }
     } catch (error) {
-      console.error(error);
       return next(new ErrorHandler(error, 400));
     }
   })
 );
 
-
-
-// get all products of a shop
-router.get(
-  "/get-all-products-shop/:id",
-  catchAsyncErrors(async (req, res, next) => {
-    try {
-      const products = await Product.find({ shopId: req.params.id });
-
-      res.status(201).json({
-        success: true,
-        products,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error, 400));
-    }
-  })
-);
 
 // // delete product of a shop
 // router.delete(
