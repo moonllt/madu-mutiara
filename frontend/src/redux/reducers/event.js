97ab29e1718cdb953e00bb71912d3c5984831633
unknown <kinantisaaz@gmail.com>
2023-08-17 15:27:27 +0700

    ...

--- frontend/src/redux/reducers/event.js
+++ frontend/src/redux/reducers/event.js
@@ -19,18 +19,18 @@ export const eventReducer = createReducer(initialState, {
     state.success = false;
   },
 
-  // // get all events of shop
-  // getAlleventsShopRequest: (state) => {
-  //   state.isLoading = true;
-  // },
-  // getAlleventsShopSuccess: (state, action) => {
-  //   state.isLoading = false;
-  //   state.events = action.payload;
-  // },
-  // getAlleventsShopFailed: (state, action) => {
-  //   state.isLoading = false;
-  //   state.error = action.payload;
-  // },
+  // get all events of shop
+  getAlleventsShopRequest: (state) => {
+    state.isLoading = true;
+  },
+  getAlleventsShopSuccess: (state, action) => {
+    state.isLoading = false;
+    state.events = action.payload;
+  },
+  getAlleventsShopFailed: (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
+  },
 
   // delete event of a shop
   deleteeventRequest: (state) => {
@@ -45,20 +45,20 @@ export const eventReducer = createReducer(initialState, {
     state.error = action.payload;
   },
 
-  // // get all events 
-  // getAlleventsRequest: (state) => {
-  //   state.isLoading = true;
-  // },
-  // getAlleventsSuccess: (state, action) => {
-  //   state.isLoading = false;
-  //   state.allEvents = action.payload;
-  // },
-  // getAlleventsFailed: (state, action) => {
-  //   state.isLoading = false;
-  //   state.error = action.payload;
-  // },
+  // get all events 
+  getAlleventsRequest: (state) => {
+    state.isLoading = true;
+  },
+  getAlleventsSuccess: (state, action) => {
+    state.isLoading = false;
+    state.allEvents = action.payload;
+  },
+  getAlleventsFailed: (state, action) => {
+    state.isLoading = false;
+    state.error = action.payload;
+  },
 
-  // clearErrors: (state) => {
-  //   state.error = null;
-  // },
+  clearErrors: (state) => {
+    state.error = null;
+  },
 });
